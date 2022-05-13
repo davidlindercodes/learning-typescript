e4fda189c08471799ef1edc459f3d0649ff3f3c2
David Linder <56964108+davidlindercodes@users.noreply.github.com>
2022-05-14 00:25:37 +0100

    logout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,15 +4,20 @@ import Menu from './Menu';
 import Nav from './Nav';
 import axios from 'axios';
 import { Navigate } from 'react-router-dom';
+import { User } from '../models/user';
 
 const Layout = (props: any) => {
     const [redirect, setRedirect] = useState(false);
+    const [user, setUser] =  useState<User | null>(null);
+
     useEffect( () => {
         (
             async () => {
                 try{
                     const {data} = await axios.get('user');
                     console.log(data)
+
+                    setUser(data);
                 } catch (error) {
                     setRedirect(true)
                 }
@@ -26,7 +31,7 @@ const Layout = (props: any) => {
 
     return (
         <div>
-            <Nav />
+            <Nav user={user}/>
             <div className="container-fluid">
                 <div className="row">
                     <Menu />
